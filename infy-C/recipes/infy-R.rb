42a58f27d8e76d3c31aa71b70975d664f6caf40b
bharati47 <61975330+bharati47@users.noreply.github.com>
2020-03-12 13:10:40 +0530

    updating the recipe

--- infy-C/recipes/infy-R.rb
+++ infy-C/recipes/infy-R.rb
@@ -3,3 +3,30 @@
 # Recipe:: infy-R
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+file "/1086304-file" do
+  content "Hello Everyone.."
+  action :create
+end
+package "httpd" do
+  action :install
+end
+service "httpd" do
+  action [ :enable, :start ]
+end
+package "tree" do
+  action :install
+end
+file "/var/www/html/index.html" do
+  content "Hello Infoscions.. Welcome to Configuration Management with Chef.. :)"
+  action :create
+end
+file '/robofile' do
+  content "This is to get Attributes
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}"
+  owner 'root'
+  group 'root'
+  action :create
+end
